7e354594473bf1f7fc89560ac960ec40bd042970
GabrielKran <gabrielkran@gmail.com>
2025-06-02 16:54:52 -0300

    Projeto parcialmente pronto, botao de remover ainda pendente

--- script.js
+++ script.js
@@ -26,7 +26,46 @@ const buttonCheck = document.getElementById('check');
 const buttonNoCheck = document.getElementById('no-check');
 
 function addBookToLibrary(book) {    
-    
+    const cardsDiv = document.querySelector('.cards');
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+        <div class="img">
+            <img src="imgs/x.svg" alt="x" id="exit-card">
+        </div>
+        <h2>${book.title}</h2>
+        <p><strong>${book.author}</strong></p>
+        <p>${book.pages} pág.</p>
+        <div class="buttons">
+            <button class="check ${book.read === 'finished'? 'check-no-view': ''}">✓</button>
+            <button class="no-check ${book.read !== 'finished' ? 'check-no-view': ''}">X</button>
+        </div>
+    `
+
+    const checkButton = card.querySelector('.check');
+    const noCheckButton = card.querySelector('.no-check');
+
+    checkButton.addEventListener('click', () => {
+        checkButton.classList.add('check-no-view');
+        noCheckButton.classList.remove('check-no-view');
+        book.read = 'toRead';
+        console.log(myLibrary);
+    })
+
+    noCheckButton.addEventListener('click', () => {
+        noCheckButton.classList.add('check-no-view');
+        checkButton.classList.remove('check-no-view');
+        book.read ='finished';
+        console.log(myLibrary);
+    })
+
+    cardsDiv.appendChild(card);
+
+    const removeCard = card.getElementById('exit-card');
+    removeCard.addEventListener('click', () => {
+        
+    })
 }
 
 function viewAndExitForm() {
@@ -39,21 +78,6 @@ function viewAndExitForm() {
         layout.classList.remove('blur-all');
         mainForm.classList.add('view-form');
     })
-}
-
-function checkNoCheck(value) {
-    if (value === 'finished') {
-        buttonCheck.addEventListener('click', () => {
-        buttonCheck.classList.add('check-no-view');
-        buttonNoCheck.classList.remove('check-no-view');
-        })
-
-    } else {
-        buttonNoCheck.addEventListener('click', () => {
-        buttonNoCheck.classList.add('check-no-view');
-        buttonCheck.classList.remove('check-no-view');
-        })
-    }
 }
 
 function formInputs() {
